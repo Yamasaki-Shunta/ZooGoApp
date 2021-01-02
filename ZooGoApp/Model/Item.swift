cb00ff7217b970e3a597e1d8346f11440c1fe6c1
Ymasaki-Sunta <syunta@gmail.com>
2021-01-02 16:09:03 +0900

    Itemモデル追加#5

---
+++ ZooGoApp/Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  NewsTest
+//
+//  Created by 山﨑隼汰 on 2020/11/24.
+//
+
+import Foundation
+
+class NewsItems {
+    
+    var title:String?
+    var url:String?
+    var pubDate:String?
+
+}
